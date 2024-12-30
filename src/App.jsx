92e794d9ed1bda62d95602a096a1155682501466
MostafaHussien2000 <mabozeed70@gmail.com>
2024-12-30 02:00:13 +0200

    supabase integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import supabase from "./supabase/client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    checkUser();
+
+    window.addEventListener("hashchange", () => {
+      checkUser();
+    });
+  }, []);
+
+  const checkUser = async () => {
+    const userData = await supabase.auth.getUser();
+    console.log(userData);
+    setUser(userData?.data?.user?.user_metadata);
+  };
+
+  const signInWithGithub = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+  };
+
+  const signout = async () => {
+    await supabase.auth.signOut();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello {user ? user.preferred_username : null},</h1>
+      {user ? (
+        <button onClick={signout}>Sign out</button>
+      ) : (
+        <button onClick={signInWithGithub}>Sign in</button>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
