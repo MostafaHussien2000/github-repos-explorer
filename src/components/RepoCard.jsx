defc07c522415aaea7f28cee3392525f041f40ed
MostafaHussien2000 <mabozeed70@gmail.com>
2024-12-30 16:13:50 +0200

    rendering all repos

---
+++ src/components/RepoCard.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Link,
+  Popover,
+  Separator,
+  Text,
+  TextArea,
+} from "@radix-ui/themes";
+import { GoComment, GoRepo, GoRepoForked, GoStar } from "react-icons/go";
+import styles from "./RepoCard.module.css";
+import { useAuth } from "../context/AuthContext";
+
+function RepoCard({ repo }) {
+  return (
+    <Card className={`${styles["repo-card"]}`}>
+      <Flex justify="between" align="center">
+        <Flex gap="4" align="center">
+          <GoRepo />
+          <Link href={repo.html_url} target="_blank">
+            {repo.name}
+          </Link>
+        </Flex>
+        <Flex gap={"2"}>
+          <Badge variant="outline">
+            <GoRepoForked />
+            {repo.forks_count}
+          </Badge>
+          <Badge variant="outline" color="yellow">
+            <GoStar />
+            {repo.stargazers_count}
+          </Badge>
+        </Flex>
+      </Flex>
+      <Separator my="3" size="4" />
+      {repo.description ? (
+        <Text size={"2"} className="desc">
+          {repo.description}
+        </Text>
+      ) : (
+        <Text size={"2"} className="no-desc" style={{ opacity: 0.4 }}>
+          There is no description for this repo.
+        </Text>
+      )}
+      <Grid columns={"2"} gap="3" className="action-btns" mt={"4"}>
+        <CommentButton />
+        <Button variant="surface" color="gray" style={{ cursor: "pointer" }}>
+          <Link href={repo.html_url} target="_blank">
+            View in Github
+          </Link>
+        </Button>
+      </Grid>
+    </Card>
+  );
+}
+
+export default RepoCard;
+
+function CommentButton() {
+  const { user } = useAuth();
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger>
+        <Button variant="soft">
+          <GoComment />
+          Comment
+        </Button>
+      </Popover.Trigger>
+      <Popover.Content width="360px">
+        <Flex gap="3">
+          <Avatar size="2" src={user.avatar_url} fallback="A" radius="full" />
+          <Box flexGrow="1">
+            <TextArea
+              required
+              placeholder="Write a comment…"
+              style={{ height: 80 }}
+            />
+            <Flex gap="3" mt="3" justify="between">
+              <Popover.Close>
+                <Button size="1">Comment</Button>
+              </Popover.Close>
+            </Flex>
+          </Box>
+        </Flex>
+      </Popover.Content>
+    </Popover.Root>
+  );
+}
--- src/pages/Repositories.jsx
+++ src/pages/Repositories.jsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Repositories.module.css";
-import { Avatar, Box, Button, Flex, Heading, Text } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
 import { useAuth } from "../context/AuthContext";
+import RepoCard from "../components/RepoCard";
 
 function Repositories() {
   const { user, signout } = useAuth();
-  console.log(user);
+
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  if (!user)
+    return (
+      <header>
+        <p>You need to login first</p>
+      </header>
+    );
+
+  useEffect(() => {
+    const getRepos = async () => {
+      try {
+        setLoading(true);
+
+        const response = await fetch(
+          `https://api.github.com/users/${user.preferred_username}/repos?per_page=8`
+        );
+        const data = await response.json();
+
+        console.log(data);
+        setRepos(data);
+      } catch (err) {
+        setError(err.message);
+        console.error("Error fetching your repos:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getRepos();
+  }, []);
+
   return (
     <main id="repositories-page" className={`${styles.repositories}`}>
       <header>
@@ -23,7 +66,24 @@ function Repositories() {
         <Heading size={9}>
           Welcome back <span>{user.preferred_username}</span>
         </Heading>
+        <Text as="p" mb={"5"}>
+          These are all repos in your account.
+        </Text>
       </section>
+
+      {loading ? (
+        <p>Loading repos ...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : repos && repos.length > 0 ? (
+        <Grid columns="2" gap="3">
+          {repos.map((repo) => (
+            <RepoCard repo={repo} key={repo.name} />
+          ))}
+        </Grid>
+      ) : (
+        <p>No repos found</p>
+      )}
     </main>
   );
 }
